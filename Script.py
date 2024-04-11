3a0519814f853e62719c9d2e7aceaf1fb51f7118
mxmoder <154695978+mxmoder@users.noreply.github.com>
2024-04-11 21:53:43 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -202,7 +202,7 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 <b> Size ⚙️: {file_size}</b>""" 
 
     IMDB_TEMPLATE_TXT = """
-<b>🏷 Title</b>: <a href={url}>**{title}**</a>
+<b>🏷 Title</b>: <a href={url}>`{title}`</a>
 **🎭 Genres: {genres}**
 📆** Year: <a href={url}/releaseinfo>{year}</a>**
 🌟** Rating: <a href={url}/ratings>{rating}</a>/10</b>**"""
